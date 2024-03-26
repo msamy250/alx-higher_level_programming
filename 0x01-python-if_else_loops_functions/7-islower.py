f84d1a1e75abe972b972a0c5607db53f1f6097de
Mahmoud Samy <mahmoudsamy@192.168.1.15>
2024-03-26 02:30:27 +0200

    update

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def islower(c):
     char = ord(c)
-    if char in range (96, 123): 
+    if char in range(96, 123):
         return(True)
     else:
         return(False)
+    
