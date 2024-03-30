1bedd571be45c0185490540e3525215747a585f7
Mahmoud Samy <mahmoudsamy@192.168.1.15>
2024-03-30 02:02:56 +0200

    update

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
 def print_last_digit(number):
+  if number >= 0:
     x = number % 10
     return (x)
+  elif number == 0:
+    return (x)
+  else :
+    x = abs(number) % 10
+    return (-x)
+    
+  
