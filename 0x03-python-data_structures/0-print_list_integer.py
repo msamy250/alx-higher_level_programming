55d13bad782ff971a2c51d997bcf545a2dfb366c
Mahmoud Samy <mahmoudsamy@192.168.1.15>
2024-03-31 06:30:40 +0200

    update

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-import random
-number = random.randint(-10, 10)
-if number > 0:
-    print(number, "is positive")
-elif number == 0:
-    print(number, "is zero")
-else:
-    print(number, "is negative")
+def print_list_integer(my_list=[]):
+    for i in my_list :
+        print("{:d}".format(i))
+        print ("")
