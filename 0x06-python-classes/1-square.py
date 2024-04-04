f19c3b50b165066839d0860b2b30db4499eb730b
Mahmoud Samy <mahmoudsamy@192.168.1.15>
2024-04-04 04:42:25 +0200

    update

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Square Module"""
+
+
+class Square:
+    """Define a square"""
+
+    def _init_(self, size=None):
+        """Constructor
+            args: size
+        """
+        self.__size = size
