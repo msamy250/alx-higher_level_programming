74aebb0433b3ccb287881415a42862fed93423ea
Mahmoud Samy <mahmoudsamy@Mahmouds-MacBook-Pro.local>
2024-04-13 12:02:47 +0200

    update

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -2,22 +2,33 @@
 """Defines a class Square"""
 class Square:
     """
-    Class that defines properties of square by: (based on 0-square.py).
-    Attribuites : 
-        Size : is a size of one side in square (1 side)
+    Class that defines properties of a square (based on 0-square.py).
+    Attributes:
+        size (int): Size of one side of the square.
     """
-    def __init__(self, size):
-            """constructe the class
-            Args:
-                Size: size of the square.
-            """
-    def size(self, value):
-        """ Set the size of the square 
+    def __init__(self, size=0):
+            """ Construct the class.
         Args:
-        Value (int) = New size of the square
+            size (int): Initial size of the square.
+            """
+        self.__size = size
+    
+    @property
+    def size(self):
+        """
+        Get the size of the square.
+        Returns:
+            int: The current size of the square.
+        """
+        return self.__size
+    @size.setter
+    def size(self, value):
+        """ Set the size of the square.
+        Args:
+            value (int): New size of the square.
         """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
-            self.__size = size
+        self.__size = value
