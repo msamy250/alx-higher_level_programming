83cf318ce16ce0b9bf04895e07d80c5d3d574817
Mahmoud Samy <mahmoudsamy@192.168.1.8>
2024-04-17 01:54:51 +0200

    update

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""defining function"""
+
+
+def read_file(filename=""):
+    """function that reads a text file"""
+    with open (filename, encoding="utf-8") as f:
+        print(f.read, end ="")
