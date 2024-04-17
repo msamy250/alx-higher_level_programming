26db09c24723f949456ddf5004dd22eb0d942775
Mahmoud Samy <mahmoudsamy@192.168.1.8>
2024-04-17 02:54:17 +0200

    update

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""defining function"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """function that writes an Object to a text file,
+    using a JSON representation
+    """
+    with open(filename, 'r',  encoding='utf-8') as f:
+        return json.load(f)
