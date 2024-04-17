a90cc2f5d685ef119004ca33bbb263aa332ad95e
Mahmoud Samy <mahmoudsamy@192.168.1.8>
2024-04-17 02:21:17 +0200

    update

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,4 +6,4 @@ def write_file(filename="", text=""):
     """function that write a text file"""
     with open(filename, 'w', encoding="utf-8") as f:
         f.write(text)
-        return len(filename)
+        return len(text)
