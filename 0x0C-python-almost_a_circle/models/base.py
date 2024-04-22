09471de8c7fd31d1e4e63bb77687f1a64387b817
Mahmoud Samy <mahmoudsamy@192.168.1.8>
2024-04-22 23:26:15 +0200

    update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 """Base model"""
 
+
 class Base:
     """Class that defines properties of square.
     """
     __nb_objects = 0
-    
+
     def __init__(self, id=None):
         """Construct the class.
         Args:
             id: id of the object.
         """
-        if id is not None :
+        if id is not None:
             self.id = id
-        else :
+        else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
