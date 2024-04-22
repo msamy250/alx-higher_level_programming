6ae2b254f5a4f1ab7f5d86c911070fb890bbf6d0
Mahmoud Samy <mahmoudsamy@192.168.1.8>
2024-04-22 23:27:29 +0200

    update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -3,9 +3,11 @@
 
 from models.base import Base
 
+
 class Rectangle(Base):
     """Class that defines properties of rectangle.
     """
+
     def __init__(self, width, height, x=0, y=0, id=None):
         """Construct the class.
         Args:
@@ -20,13 +22,13 @@ class Rectangle(Base):
         self.height = height
         self.x = x
         self.y = y
-    
+
     @property
     def width(self):
         """Returns the width of the rectangle
         """
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         """Set the width of rectangle to new value with validation"""
@@ -35,13 +37,13 @@ class Rectangle(Base):
         elif value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-    
+
     @property
     def height(self):
         """Returns the height of the rectangle
         """
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         """Set the height of rectangle to new value with validation"""
@@ -50,13 +52,13 @@ class Rectangle(Base):
         elif value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-    
+
     @property
     def x(self):
         """Returns the x position of the rectangle
         """
         return self.__x
-    
+
     @x.setter
     def x(self, value):
         """Set the x position of rectangle to new value with validation"""
@@ -65,13 +67,13 @@ class Rectangle(Base):
         elif value < 0:
             raise ValueError("x must be >= 0")
         self.__x = value
-    
+
     @property
     def y(self):
         """Returns the y position of the rectangle
         """
         return self.__y
-    
+
     @y.setter
     def y(self, value):
         """Set the y position of rectangle to new value with validation"""
@@ -80,7 +82,7 @@ class Rectangle(Base):
         elif value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
-    
+
     def area(self):
         """Return the area of the rectangle.
         """
@@ -89,7 +91,7 @@ class Rectangle(Base):
     def display(self):
         """Prints in stdout the rectangle with the character #.
         """
-        for i in range (self.height):
+        for i in range(self.height):
             print("#" * self.width)
 
     def __str__(self):
