71f60338002ee9d950effbab454f9427990de3ca
Mahmoud Samy <mahmoudsamy@192.168.1.14>
2024-06-20 12:56:09 +0300

    update

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,9 +2,9 @@
 /* print argv */
 
 if (process.argv.length === 2) {
-    console.log('No argument');
+  console.log('No argument');
 } else if (process.argv.length === 3) {
-    console.log('Argument found');
+  console.log('Argument found');
 } else if (process.argv.length > 3) {
-    console.log('Arguments found');
+  console.log('Arguments found');
 }
