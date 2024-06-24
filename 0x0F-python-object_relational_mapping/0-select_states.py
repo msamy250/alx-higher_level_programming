adadd348518f1158df9604f151f1a220a4a45868
Mahmoud Samy <mahmoudsamy@192.168.1.14>
2024-06-24 13:11:49 +0300

    update

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,25 +1,18 @@
 #!/usr/bin/python3
 import MySQLdb
-import sys
 
-if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
 
-    # Connect to the MySQL server running on localhost at port 3306
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+db = MySQLdb.connect(
+    host="localhost",
+    user="yourusername",
+    passwd="yourpassword",
+    db="testdb"
+)
 
-    cursor = db.cursor()
 
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+cursor = db.cursor()
+cursor.execute("SELECT VERSION()")
+data = cursor.fetchone()
+print(f"MySQL version: {data}")
 
-    states = cursor.fetchall()
-
-    # Print the results
-    for state in states:
-        print(state)
-
-    # Close the cursor and the connection
-    cursor.close()
-    db.close()
+db.close()
